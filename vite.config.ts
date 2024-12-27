c6e21afd0d4186d627c664099c0281e0a6aad469
Aristidescosta200 <aristidescosta330@gmail.com>
2024-12-27 02:55:17 -0800

    chore!: initial configuration and folder structure

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import * as path from "path";
+
+// https://vite.dev/config/
+export default defineConfig({
+  resolve: {
+		alias: {
+			"@": path.resolve("app"),
+			"@styles": path.resolve("app/src/styles"),
+			"@services": path.resolve("app/src/services"),
+			"@components": path.resolve("app/src/components"),
+			"@pages": path.resolve("app/src/pages"),
+			"@hooks": path.resolve("app/src/hooks"),
+			"@database": path.resolve("app/src/database"),
+			"@routes": path.resolve("app/src/routes/"),
+			"@types": path.resolve("app/src/types"),
+			"@repositories": path.resolve("app/src/repositories"),
+			"@utils": path.resolve("app/src/utils"),
+			"@store": path.resolve("app/src/store"),
+		},
+	},
+  plugins: [react()],
+})
