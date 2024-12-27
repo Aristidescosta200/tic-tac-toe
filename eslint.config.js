b1df75e59069fb729c7fecac398d6b0725309b9c
Aristidescosta200 <aristidescosta330@gmail.com>
2024-12-27 03:17:08 -0800

    feat!: adding eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,31 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      "react/react-in-jsx-scope": "off", // Desabilita a regra que exige React no escopo
+      "no-unused-vars": "warn",
+      "no-undef": "warn",
+      "eqeqeq": "error",
+      "no-console": "warn", // Avise sobre console.log, pode ser útil apenas em debug
+      "curly": "error", // Exige o uso de chaves em blocos (if, for, etc.)
+      "no-shadow": "warn", // Evita variáveis que sombreiam outras no escopo externo
+      "no-var": "error", // Incentiva o uso de let/const em vez de var
+      "prefer-const": "warn", // Sugere const para variáveis não reassinadas
+      "no-debugger": "warn", // Evita deixar o debugger no código
+      "arrow-body-style": ["warn", "as-needed"], // Usa corpo de arrow functions apenas quando necessário
+
     },
-  },
-)
+  }
+];
