ff13e125cbdbff98b85e583dca7dadea6daa7e9e
Aristidescosta200 <aristidescosta330@gmail.com>
2024-12-27 07:05:54 -0800

    feat!: loading screen implementation with a fake timer

--- app/src/screens/SplashScreen/index.tsx
+++ app/src/screens/SplashScreen/index.tsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router'
 
 export const SplashScreen = () => {
-  return (
-    <div>SplashScreen</div>
-  )
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const timeoutId = setTimeout(() => {
+            navigate("welcome");
+        }, 3000);
+
+        return () => {
+            clearTimeout(timeoutId); // Limpa o timeout se o componente for desmontado
+        };
+    }, [])
+
+    return (
+        <div className='h-screen w-screen flex flex-col items-center justify-center'>
+            <img src="Logo.png" className='motion-preset-pulse' alt="Logo do jogo" />
+            <p className='mt-5'>Carregando...</p>
+        </div>
+    )
 }
