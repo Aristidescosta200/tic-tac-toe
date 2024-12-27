1fae5f57633105fe670813143d48f7c6326fc4c2
Aristidescosta200 <aristidescosta330@gmail.com>
2024-12-27 06:49:51 -0800

    chore!: tailwind css installation and configuration

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,6 +1,8 @@
 import { createRoot } from "react-dom/client";
 import { StrictMode } from "react";
 
+import "@styles/global.css";
+
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./app/src/**/*.{js,ts,jsx,tsx}",],
+  theme: {
+    extend: {
+      colors: {
+        text: "var(--color-text)",
+        background: "var(--color-background)",
+      },
+    },
+  },
+  plugins: [],
+}
+
